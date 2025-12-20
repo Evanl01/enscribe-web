9d5be9ce0331500e7445e5beaa3ab99f0f2865a4
Evan Loo <evanhsie@usc.edu>
2025-12-20 17:04:36 -0500

    Finished Nextjs -> React migration. Delete redundant app/ folder and its files. (migrated to pages/ folder)

--- src/app/schemas/index.js
+++
@@ -1,6 +0,0 @@
-export { soapNoteSchema } from '@/src/app/schemas/soapNote';
-export { recordingSchema } from '@/src/app/schemas/recording';
-export { transcriptSchema } from '@/src/app/schemas/transcript';
-export { patientEncounterSchema } from '@/src/app/schemas/patientEncounter';
-export { dotPhraseSchema } from '@/src/app/schemas/dotPhrase';
-// Add more schemas as needed
